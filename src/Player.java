fa46f09249bd36f1f2f66f2acaeec6d4585fe376
nicolas bodelle <nicolas.bodelle@mypangee.com>
2022-12-12 19:50:40 +0100

    move around if no control and only one recycler turn 1

--- src/Player.java
+++ src/Player.java
@@ -11,9 +11,12 @@ class Player {
         Scanner in = new Scanner(System.in);
         int width = in.nextInt();
         int height = in.nextInt();
+        Tile[][] map = new Tile[width][height];
+        int loop = 0;
 
         // game loop
         while (true) {
+            loop++;
             List<Tile> tiles = new ArrayList<>();
             List<Tile> myTiles = new ArrayList<>();
             List<Tile> oppTiles = new ArrayList<>();
@@ -25,11 +28,13 @@ class Player {
 
             int myMatter = in.nextInt();
             int oppMatter = in.nextInt();
+            // ------------------------------------------------- input data -------------------------------------------------
             for (int y = 0; y < height; y++) {
                 for (int x = 0; x < width; x++) {
                     Tile tile = new Tile(x, y, in.nextInt(), in.nextInt(), in.nextInt(), in.nextInt() == 1,
                         in.nextInt() == 1, in.nextInt() == 1, in.nextInt() == 1);
                     tiles.add(tile);
+                    map[x][y] = tile;
 
                     if (tile.getOwner() == ME) {
                         myTiles.add(tile);
@@ -50,33 +55,52 @@ class Player {
                     }
                 }
             }
+            // ------------------------------------------------- input data -------------------------------------------------
 
+            // ------------------------------------------------- action -------------------------------------------------
             List<String> actions = new ArrayList<>();
 
             for (Tile tile : myTiles) {
+                // -------- spawn ----------
                 if (tile.isCanSpawn()) {
-                    int amount = 0; // TODO: pick amount of robots to spawn here
-                    if (amount > 0) {
-                        actions.add(String.format("SPAWN %d %d %d", amount, tile.getX(), tile.getY()));
+                    // matter + 10 -> spawn
+                    if (myMatter > 10) {
+                        actions.add(String.format("SPAWN %d %d %d", 1, tile.getX(), tile.getY()));
                     }
+
                 }
+                // -------- build ----------
                 if (tile.isCanBuild()) {
-                    boolean shouldBuild = false; // TODO: pick whether to build recycler here
+                    // recycler loop 1 only
+                    boolean shouldBuild = false;
+                    if (loop == 1) {
+                        shouldBuild = true;
+                    }
                     if (shouldBuild) {
                         actions.add(String.format("BUILD %d %d", tile.getX(), tile.getY()));
                     }
                 }
             }
 
+            // -------- move ----------
             for (Tile tile : myUnits) {
-                Tile target = new Tile(oppUnits.get(0).getX(),oppUnits.get(0).getY()); // TODO: pick a destination
-                if (target != null) {
-                    int amount = 1; // TODO: pick amount of units to move
-                    actions.add(String.format("MOVE %d %d %d %d %d", amount, tile.getX(), tile.getY(), target.getX(), target.getY()));
+                // move around with scrapAmount and no control
+                if ((tile.getX() - 1) >= 0 && map[tile.getX() - 1][tile.getY()].getScrapAmount() > 0 && map[tile.getX() - 1][tile.getY()].getOwner() < 1) {
+                    actions.add(String.format("MOVE %d %d %d %d %d", tile.getUnits(), tile.getX(), tile.getY(), tile.getX() - 1, tile.getY()));
+                } else if ((tile.getY() - 1) >= 0 && map[tile.getX()][tile.getY() - 1].getScrapAmount() > 0 && map[tile.getX()][tile.getY() - 1].getOwner() < 1) {
+                    actions.add(String.format("MOVE %d %d %d %d %d", tile.getUnits(), tile.getX(), tile.getY(), tile.getX(), tile.getY() - 1));
+                } else if ((tile.getX() + 1) < width && map[tile.getX() + 1][tile.getY()].getScrapAmount() > 0 && map[tile.getX() + 1][tile.getY()].getOwner() < 1) {
+                    actions.add(String.format("MOVE %d %d %d %d %d", tile.getUnits(), tile.getX(), tile.getY(), tile.getX() + 1, tile.getY()));
+                } else if ((tile.getY() + 1) < height && map[tile.getX()][tile.getY() + 1].getScrapAmount() > 0 && map[tile.getX()][tile.getY() + 1].getOwner() < 1) {
+                    actions.add(String.format("MOVE %d %d %d %d %d", tile.getUnits(), tile.getX(), tile.getY(), tile.getX(), tile.getY() + 1));
+                } else {
+                    Tile target = new Tile(width/2, height/2);
+                    actions.add(String.format("MOVE %d %d %d %d %d", tile.getUnits(), tile.getX(), tile.getY(), target.getX(), target.getY()));
                 }
             }
 
-            // To debug: System.err.println("Debug messages...");
+            // ------------------------------------------------- result -------------------------------------------------
+
             if (actions.isEmpty()) {
                 System.out.println("WAIT");
             } else {
---
+++ src/Utils.java
@@ -0,0 +1,4 @@
+import java.util.List;
+
+abstract class Utils {
+}
